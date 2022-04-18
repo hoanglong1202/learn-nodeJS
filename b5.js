b49509b4588f5685c18a994a1f1a4c22616355a4
Vo Dinh Hoang Long <cyberpunk2076@gmail.com>
2022-04-18 16:58:05 +0700

    add b5

---
+++ b5.js
@@ -0,0 +1,38 @@
+// for loop
+// có thể dùng break hoặc continue
+// Các parameter là biến iterator, counter và incrementor.
+// thực thi tốt với async/ await
+
+// -------------------------------------------
+// forEach
+// không thể dùng từ khóa break hoặc continue vì forEach gọi các callback function
+// Các parameter là biến iterator, index of item và array to iterate.
+// giữ phạm vi của biến trong block
+const num = 4;
+const arr = [0, 1, 2];
+
+arr.forEach(num => {
+  console.log(num);
+});
+
+// Result: 0 1 2
+console.log(num);
+
+// Result: 4
+// Không thích hợp với async/ await vì forEach không chờ asynchronous process trong callback
+let count = 0;
+hello = async (items) => {
+  items.forEach(async () => {
+    await someAPICall();
+    count++;
+  })
+  console.log("count = " + count);
+}
+someAPICall = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("done")
+    }, 100);
+  })
+}
+hello(['1', '2', '3', '4']);
--- b7.js
+++ b7.js
@@ -7,12 +7,14 @@ const controller = (lopp) => {
   console.log("end loop" + lopp);
 }
 
+// a promise-wrapper function so that it don't block the current stack
 function asyncLoop(arr) {
   return new Promise((res, rej) => {
     recursivelyAsyncLoop(arr, res).catch(rej);
   });
 }
 
+// when event-loop finish its phases on the current iteration, it will call the passed function inside the setImmediate on its next iteration
 async function recursivelyAsyncLoop(arr, resolve, i = 0) {
   if (i === arr.length) {
     resolve();
